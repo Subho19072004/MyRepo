1943351901b9798acb5884d52ad4d4029df9124a
Subho Das <subhodas1907@gmail.com>
2026-04-29 20:59:54 +0530

    Update greeting message in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World mckv1, I am student of mckvie')
+  res.send('Hello World mckvie')
 })
 
 app.listen(4000, ()=>{
